31d4a5f8afce8880075ee4a3b3ef1ce567b2856c
MUHAMMAD HAFIDH SHOVI <144792158+hafidsopie@users.noreply.github.com>
2025-12-23 00:55:38 +0800

    Update modeling.py

--- MLProject/modeling.py
+++ MLProject/modeling.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score, f1_score
 from xgboost import XGBClassifier
 import mlflow
 import mlflow.sklearn
@@ -10,14 +11,11 @@ DATA_PATH = "stunting_wasting_preprocessing.csv"
 EXPERIMENT_NAME = "Stunting Classification - XGBoost"
 
 def run_model():
-    mlflow.set_tracking_uri("file:./mlruns")
-    mlflow.set_experiment(EXPERIMENT_NAME)
-
-    # ✅ AUTOLOG FULL (TANPA MANUAL LOG)
-    mlflow.sklearn.autolog()
-
     print("Training dimulai...")
     print("Working directory:", os.getcwd())
+
+    mlflow.set_experiment(EXPERIMENT_NAME)
+    mlflow.sklearn.autolog()
 
     df = pd.read_csv(DATA_PATH)
     print("Data berhasil diload")
@@ -26,10 +24,7 @@ def run_model():
     y = df["Stunting"]
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y,
-        test_size=0.2,
-        random_state=42,
-        stratify=y
+        X, y, test_size=0.2, random_state=42, stratify=y
     )
 
     scaler = MinMaxScaler()
@@ -48,7 +43,16 @@ def run_model():
 
     model.fit(X_train, y_train)
 
-    print("Training selesai")
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average="weighted")
+
+    mlflow.log_metric("accuracy_manual", acc)
+    mlflow.log_metric("f1_weighted", f1)
+
+    print(f"Akurasi: {acc}")
+    print(f"F1-score: {f1}")
 
 if __name__ == "__main__":
     run_model()
