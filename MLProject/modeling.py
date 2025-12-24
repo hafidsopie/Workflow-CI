bffe1b8214a6a0c03aaf52782bafa2645f0ab5d6
MUHAMMAD HAFIDH SHOVI <144792158+hafidsopie@users.noreply.github.com>
2025-12-24 08:44:34 +0800

    Update modeling.py

--- MLProject/modeling.py
+++ MLProject/modeling.py
@@ -1,39 +1,30 @@
 import os
 import pandas as pd
+import argparse
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 
 from xgboost import XGBClassifier
-import argparse
+
 import mlflow
 import mlflow.sklearn
 
 # ===============================
 # Konfigurasi MLflow
 # ===============================
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MLRUNS_PATH = os.path.join(BASE_DIR, "mlruns")
-
-mlflow.set_tracking_uri(f"file:{MLRUNS_PATH}")
 mlflow.set_experiment("Prediksi_Balita_Stunting_Wasting_XGBoost")
-
 mlflow.sklearn.autolog()
-
-# ===============================
-# Path Dataset
-# ===============================
-DATA_PATH = os.path.join(BASE_DIR, "stunting_wasting_preprocessing.csv")
 
 # ===============================
 # Training Function
 # ===============================
-def run_model(DATA_PATH):
+def run_model(data_path):
     print("Memulai training model...")
-    print("Mencari dataset di:", DATA_PATH)
+    print("Mencari dataset di:", data_path)
 
-    df = pd.read_csv(DATA_PATH)
+    df = pd.read_csv(data_path)
     print("Dataset berhasil diload")
 
     X = df.drop("Stunting", axis=1)
@@ -66,42 +57,19 @@ def run_model(DATA_PATH):
 
     y_pred = model.predict(X_test_scaled)
     accuracy = accuracy_score(y_test, y_pred)
+
     print("Accuracy:", accuracy)
 
     mlflow.log_metric("accuracy", accuracy)
 
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=X_train.iloc[:5]
-    )
-
-    mlflow.sklearn.log_model(
-        sk_model=scaler,
-        artifact_path="scaler"
-    )
-
-    print("Training selesai dan model berhasil disimpan di MLflow.")
+    print("Training selesai dan model tersimpan di MLflow")
 
 # ===============================
 # Main
 # ===============================
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--data_path",
-        type=str,
-        required=True,
-        help="Path dataset CSV"
-    )
+    parser.add_argument("--data_path", type=str, required=True)
     args = parser.parse_args()
 
-    DATA_PATH = os.path.join(BASE_DIR, args.data_path)
-
-    os.makedirs(MLRUNS_PATH, exist_ok=True)
-    run_model(DATA_PATH)
-
-
-
-
-
+    run_model(args.data_path)
