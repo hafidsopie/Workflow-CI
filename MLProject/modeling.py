b66eab5f4a6c16fbe0377c11c5d3e1dc16e79e56
MUHAMMAD HAFIDH SHOVI <144792158+hafidsopie@users.noreply.github.com>
2025-12-24 08:40:19 +0800

    Update modeling.py

--- MLProject/modeling.py
+++ MLProject/modeling.py
@@ -30,58 +30,58 @@ DATA_PATH = os.path.join(BASE_DIR, "stunting_wasting_preprocessing.csv")
 # Training Function
 # ===============================
 def run_model(DATA_PATH):
-        print("Memulai training model...")
-        print("Mencari dataset di:", DATA_PATH)
+    print("Memulai training model...")
+    print("Mencari dataset di:", DATA_PATH)
 
-        df = pd.read_csv(DATA_PATH)
-        print("Dataset berhasil diload")
+    df = pd.read_csv(DATA_PATH)
+    print("Dataset berhasil diload")
 
-        X = df.drop("Stunting", axis=1)
-        y = df["Stunting"]
+    X = df.drop("Stunting", axis=1)
+    y = df["Stunting"]
 
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y,
-            test_size=0.2,
-            random_state=42,
-            stratify=y
-        )
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
 
-        scaler = StandardScaler()
-        X_train_scaled = scaler.fit_transform(X_train)
-        X_test_scaled = scaler.transform(X_test)
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
-        model = XGBClassifier(
-            n_estimators=300,
-            learning_rate=0.1,
-            max_depth=6,
-            subsample=0.8,
-            colsample_bytree=0.8,
-            objective="multi:softmax",
-            eval_metric="mlogloss",
-            random_state=42
-        )
+    model = XGBClassifier(
+        n_estimators=300,
+        learning_rate=0.1,
+        max_depth=6,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        objective="multi:softmax",
+        eval_metric="mlogloss",
+        random_state=42
+    )
 
-        print("Training model XGBoost...")
-        model.fit(X_train_scaled, y_train)
+    print("Training model XGBoost...")
+    model.fit(X_train_scaled, y_train)
 
-        y_pred = model.predict(X_test_scaled)
-        accuracy = accuracy_score(y_test, y_pred)
-        print("Accuracy:", accuracy)
+    y_pred = model.predict(X_test_scaled)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
 
-        mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("accuracy", accuracy)
 
-        mlflow.sklearn.log_model(
-            sk_model=model,
-            artifact_path="model",
-            input_example=X_train.iloc[:5]
-        )
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        input_example=X_train.iloc[:5]
+    )
 
-        mlflow.sklearn.log_model(
-            sk_model=scaler,
-            artifact_path="scaler"
-        )
+    mlflow.sklearn.log_model(
+        sk_model=scaler,
+        artifact_path="scaler"
+    )
 
-        print("Training selesai dan model berhasil disimpan di MLflow.")
+    print("Training selesai dan model berhasil disimpan di MLflow.")
 
 # ===============================
 # Main
@@ -100,6 +100,7 @@ if __name__ == "__main__":
 
     os.makedirs(MLRUNS_PATH, exist_ok=True)
     run_model(DATA_PATH)
+
 
 
 
